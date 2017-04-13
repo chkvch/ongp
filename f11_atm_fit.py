21e5c48671cbd272ea5f525911b0cfac00789c75
Christopher Mankovich <cmankovich@ucsc.edu>
2017-04-12 21:35:43 -0700

    reos.py and f11_atm_fit.py to git

---
+++ f11_atm_fit.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from scipy.optimize import brentq
+
+class atm:
+    """a class to implement the [F11] model atmospheres for Jupiter and Saturn,
+    as fit analytically by [LC13].
+    
+    [F11] Fortney, J. J., Ikoma, M., Nettelmann, N., Guillot, T., & Marley, M. S. 2011,
+    ApJ, 729, 32
+    
+    [LC13] Leconte, J., & Chabrier, G. 2013, Nature Geoscience, 6, 347
+    
+    """
+    
+    def __init__(self, planet='jup', print_table=False):
+        assert planet in ['jup', 'sat'], 'planet label %s not recognized. choose from jup, sat' % planet
+        self.planet = planet
+        
+        if planet is 'jup':
+            self.clo = 78.4e0
+            self.klo = 0.0055e0
+            self.t0lo = -122e0
+            self.betalo = -0.182e0
+            self.gammalo = 2.09e0
+
+            self.chi = -283e0
+            self.khi = 198e0
+            self.t0hi = 143e0
+            self.betahi = -0.114e0
+            self.gammahi = 0.454e0
+
+            self.tmid = 224e0
+            self.dt = 25e0
+        elif planet is 'sat':
+			self.clo = 62.7e0
+			self.klo = 0.00202e0
+			self.t0lo = -97.1e0
+			self.betalo = -0.18e0
+			self.gammalo = 2.31e0
+			
+			self.chi = -1680e0
+			self.khi = 831e0
+			self.t0hi = 141e0
+			self.betahi = -0.0721e0
+			self.gammahi = 0.293e0
+			
+			self.tmid = 225e0
+			self.dt = 25e0
+            
+    def get_t10(self, g, tint):
+        
+        if tint > self.tmid + self.dt:
+            t10hi = self.chi + self.khi * g ** self.betahi * (tint - self.t0hi) ** self.gammahi
+            return t10hi
+        elif tint < self.tmid - self.dt:
+            t10lo = self.clo + self.klo * g ** self.betalo * (tint - self.t0lo) ** self.gammalo
+            return t10lo
+        else:
+            t10hi = self.chi + self.khi * g ** self.betahi * (tint - self.t0hi) ** self.gammahi
+            t10lo = self.clo + self.klo * g ** self.betalo * (tint - self.t0lo) ** self.gammalo
+            alpha = (tint - (self.tmid - self.dt)) / (2. * self.dt) 
+            return alpha * t10hi + (1. - alpha) * t10lo
+            
+    def get_tint(self, g, t10):
+        """takes g (mks) and t10 (K) as arguments; uses the analytic fitting formula of Leconte+Chabrier2013 to return the
+        intrinsic temperature tint (K)."""
+        
+        zero_me = lambda tint_: self.get_t10(g, tint_) - t10
+        
+        t0 = 50
+        t1 = 3e3
+        if not zero_me(t0) * zero_me(t1) < 0:
+            print 'no root between bounds t0=%g, t1=%g' % (t0, t1)
+            raise ValueError
+        
+        from scipy.optimize import brentq
+        tint = brentq(zero_me, t0, t1)
+
+        return tint
---
+++ reos.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import RegularGridInterpolator
+
+class eos:
+    
+    def __init__(self):
+        path = '/Users/chris/Dropbox/planet_models/reos/reos_water_pt.dat'
+
+        self.names = 'logrho', 'logt', 'logp', 'logu', 'logs', 'chit', 'chirho', 'gamma1'
+        self.data = np.genfromtxt(path, names=self.names)
+
+        self.logpvals = np.unique(self.data['logp'])
+        self.logtvals = np.unique(self.data['logt'])
+                
+        self.nptsp = len(self.logpvals)
+        self.nptst = len(self.logtvals)
+        
+        self.logrho_on_pt = np.zeros((self.nptsp, self.nptst))
+        self.logu_on_pt = np.zeros((self.nptsp, self.nptst))
+        self.logs_on_pt = np.zeros((self.nptsp, self.nptst))
+        self.chit_on_pt = np.zeros((self.nptsp, self.nptst))
+        self.chirho_on_pt = np.zeros((self.nptsp, self.nptst))
+        self.gamma1_on_pt = np.zeros((self.nptsp, self.nptst))
+
+        for i, logpval in enumerate(self.logpvals):
+            data_this_logp = self.data[self.data['logp'] == logpval]
+            for j, logtval in enumerate(self.logtvals):
+                data_this_logp_logt = data_this_logp[data_this_logp['logt'] == logtval]
+                self.logrho_on_pt[i, j] = data_this_logp_logt['logrho']
+                self.logu_on_pt[i, j] = data_this_logp_logt['logu']
+                self.logs_on_pt[i, j] = data_this_logp_logt['logs']
+                self.chit_on_pt[i, j] = data_this_logp_logt['chit']
+                self.chirho_on_pt[i, j] = data_this_logp_logt['chirho']
+                self.gamma1_on_pt[i, j] = data_this_logp_logt['gamma1']
+
+        pt_basis = (self.logpvals, self.logtvals)
+        self._get_logrho = RegularGridInterpolator(pt_basis, self.logrho_on_pt)
+        self._get_logu = RegularGridInterpolator(pt_basis, self.logu_on_pt)
+        self._get_logs = RegularGridInterpolator(pt_basis, self.logs_on_pt)        
+        self._get_chit = RegularGridInterpolator(pt_basis, self.chit_on_pt)        
+        self._get_chirho = RegularGridInterpolator(pt_basis, self.chirho_on_pt)        
+        self._get_gamma1 = RegularGridInterpolator(pt_basis, self.gamma1_on_pt)        
+                
+    def get_logrho(self, logp, logt):
+        if np.any(logt > max(self.logtvals)):
+            raise ValueError('logt above reos bounds. max available %f, got %f.' % (self.logtvals[-1], max(logt)))
+        elif np.any(logt < min(self.logtvals)):
+            raise ValueError('logt below reos bounds. min available %f, got %f.' % (self.logtvals[0], min(logt)))
+        elif np.any(logp > max(self.logpvals)):
+            raise ValueError('logp above reos bounds. max available %f, got %f' % (self.logpvals[-1], max(logp)))
+        elif np.any(logp < min(self.logpvals)):
+            raise ValueError('logp below reos bounds. min available %f, got %f' % (self.logtvals[0], min(logp)))
+
+        return self._get_logrho((logp, logt))
+
+    def get_logu(self, logp, logt):
+        return self._get_logu((logp, logt))
+
+    def get_logs(self, logp, logt):
+        return self._get_logs((logp, logt))
+        
+    def get_chit(self, logp, logt):
+        return self._get_chit((logp, logt))
+
+    def get_chirho(self, logp, logt):
+        return self._get_chirho((logp, logt))
+
+    def get_gamma1(self, logp, logt):
+        return self._get_gamma1((logp, logt))
+        
+    def get_dlogrho_dlogp_const_t(self, logp, logt, f=8e-1):
+        logp_lo = logp - np.log10(1. - f)
+        logp_hi = logp + np.log10(1. + f)
+        logrho_lo = self.get_logrho(logp_lo, logt)
+        logrho_hi = self.get_logrho(logp_hi, logt)
+        return (logrho_hi - logrho_lo) / (logp_hi - logp_lo)
+
+    def get_dlogrho_dlogt_const_p(self, logp, logt, f=8e-1):
+        logt_lo = logt - np.log10(1. - f)
+        logt_hi = logt + np.log10(1. + f)
+        logrho_lo = self.get_logrho(logp, logt_lo)
+        logrho_hi = self.get_logrho(logp, logt_hi)
+        return (logrho_hi - logrho_lo) / (logt_hi - logt_lo)
+
+    # def get_dlogrho_dlogt_const_p(self, logp, logt):
+    #     return self.get_chit(logp, logt) / self.get_chirho(logp, logt)
+    #
+    # def get_dlogrho_dlogp_const_t(self, logp, logt):
+    #     return 1. / self.get_chirho(logp, logt)
+
+
+        
+    # def get_gamma1(self, logp, logt, f=5e-1):
+    #     '''take centered differences to compute dlogp/dlogrho at constant entropy.
+    #     uses the version of this module that takes rho, s as the thermodynamic basis.'''
+    #     logrho = self.get_logrho(logp, logt)
+    #     logs = self.get_logs(logp, logt)
+    #     import reos_rhos
+    #     rhos_eos = reos_rhos.eos()
+    #
+    #     logrho_hi = logrho + np.log10(1. + f)
+    #     logrho_lo = logrho + np.log10(1. - f)
+    #     logp_hi = rhos_eos.get_logp(logrho_hi, logs)
+    #     logp_lo = rhos_eos.get_logp(logrho_lo, logs)
+    #
+    #     return (logp_hi - logp_lo) / (logrho_hi - logrho_lo)
+    #
