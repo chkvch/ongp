8932fb5fe4df06232159c5d35c0b9c056eb675e9
Christopher Mankovich <cmankovich@ucsc.edu>
2019-01-22 09:48:25 -0800

    miscibility gap returns string for stable/fail and tuple of floats for gap

--- lorenzen.py
+++ lorenzen.py
@@ -114,7 +114,7 @@ class hhe_phase_diagram:
         assert p < phi
 
         if t > self.tcrit[plo] or t > self.tcrit[phi]:
-            return 'stable', 'stable'
+            return 'stable'
 
         dz = 1e-3
         # for abundance in helium-poor phase, search between dz and z(tcrit)
@@ -125,7 +125,7 @@ class hhe_phase_diagram:
             zlo_phi = brentq(lambda z: self.splinet(phi, z) - t, dz, self.zcrit[phi])
             zhi_phi = brentq(lambda z: self.splinet(phi, z) - t, self.zcrit[phi], 1 - dz)
         except ValueError:
-            return 'failed', 'failed'
+            return 'failed'
 
         xlo_plo = self.splinex(plo, zlo_plo)
         xhi_plo = self.splinex(plo, zhi_plo)
