3c6b0a5049aac7de73c84f38b5b06a51eee702e7
Christopher Mankovich <cmankovich@ucsc.edu>
2020-08-20 18:16:41 -0700

    small tweak to handling of data path

--- ongp.py
+++ ongp.py
@@ -14,9 +14,10 @@ class evol:
 
     def __init__(self, params, mesh_params={}):
 
-        if not 'path_to_data' in params.keys():
-            params['path_to_data'] = os.environ['ongp_data_path']
-            if not os.path.exists(params['path_to_data']):
+        if not 'path_to_data' in params:
+            if 'ongp_data_path' in os.environ:
+                params['path_to_data'] = os.environ['ongp_data_path']
+            if (not os.path.exists(params['path_to_data'])) or ('ongp_data_path' not in os.environ):
                 raise ValueError('must specify path_to_data indicating path to eos/atm data. best way is to set environment variable ongp_data_path.')
 
         if not 'hhe_eos_option' in params:
